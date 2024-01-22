b22de645dd0d7ee90a0db2a3a9adb74a4b5fb128
Ryan Salvador <rsalvador@optimizex.com>
2024-01-21 19:19:20 -0700

    Laracasts up to episode 27

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,12 +4,14 @@
   @foreach ($posts as $post)
     <article>
        <h1>
-         <a href="/posts/{{ $post->slug }}">
-           
-           {{ $post->title }}
+         <a href="/posts/{{ $post->slug }}">       
+           {!! $post->title !!}
          </a>
        </h1>
-       <div>{{ $post->excerpt }}</div>
+       <p>
+         <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+       </p>
+       <div>{!! $post->excerpt !!}</div>
     </article>
   @endforeach
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
-Route::get('/posts/{post}', function ($slug) {
+Route::get('/posts/{post:slug}', function (Post $post) {
 
     return view('post', [
-       'post' => Post::findOrFail($slug)
+       'post' => $post
     ]);
 
+});
+
+Route::get('/categories/{category:slug}', function (\App\Models\Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
 });
